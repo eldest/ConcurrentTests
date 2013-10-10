ccd150683acf0f373155859f733eac947b9d53de
eldest <eldest.007@gmail.com>
2013-10-10 15:10:40 +0600

    updated tests

---
+++ src/test/java/com/eldest/concurrent/ExecutorServiceTest.java
@@ -0,0 +1,73 @@
+package com.eldest.concurrent;
+
+import org.junit.Test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created with IntelliJ IDEA for ConcurrentTests.
+ * User: EremenkoAA
+ * Date: 10.10.13
+ *
+ * @see <a href="http://tutorials.jenkov.com/java-util-concurrent/executorservice.html">tutorials.jenkov.com</a>
+ */
+public class ExecutorServiceTest {
+
+    static final int ONE_SEC = 1000;
+    static final int TEN_SEC = 10 * 1000;
+
+    @Test
+    public void testFixedThreadPool() throws Throwable {
+        System.out.println();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        for (int i = 0; i < 10; i++) {
+            executorService.execute(new LocalThread("Thread-" + i));
+        }
+
+        executorService.shutdown();
+
+        while (!executorService.isTerminated()) {
+            System.out.print(".");
+            Thread.sleep(ONE_SEC);
+        }
+    }
+
+    @Test
+    public void testCachedThreadPool() throws Throwable {
+        System.out.println();
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 10; i++) {
+            executorService.execute(new LocalThread("Thread-" + i));
+        }
+
+        executorService.shutdown();
+
+        while (!executorService.isTerminated()) {
+            System.out.print(".");
+            Thread.sleep(ONE_SEC);
+        }
+    }
+
+    private static class LocalThread extends Thread {
+
+        private LocalThread(String name) {
+            super(name);
+        }
+
+        @Override
+        public void run() {
+            System.out.print(String.format(" %s ", getName()));
+
+            try {
+                Thread.sleep(TEN_SEC);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
